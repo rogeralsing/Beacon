7ed79f622d2c5a692a9a05abb8dc75a0f7e84f07
rogeralsing <rogeralsing@gmail.com>
2015-06-30 19:58:29 +0200

    Initial Commit

---
+++ src/Beacon/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Beacon")>]
+[<assembly: AssemblyProductAttribute("Beacon")>]
+[<assembly: AssemblyDescriptionAttribute("HTTP Service Discovery")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
